9fa70f78576813d1877f0e7515a6706298f81bb7
Dylan Chen <hjchen2005@gmail.com>
2015-11-25 16:14:56 -0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -17,8 +17,50 @@ private void button_click(object sender, EventArgs e){
 
 private void checkForWinners(){
 	//foreach(Control c in Control)
-	if (a1.Text == a2.Text) && (a2.Text == a3.Text)
+	
+	// Horizontals
+	if ((a1.Text == a2.Text) && (a2.Text == a3.Text))
 		someoneWins = true;
-	if (a1.Text == a2.Text) && (b2.Text == c3.Text)
-	if (b1.Text == b2.Text) && (a2.Text == a3.Text)
+	if ((b1.Text == b2.Text) && (b2.Text == b3.Text))
+		someoneWins = true;
+	if ((c1.Text == b2.Text) && (b2.Text == a3.Text))
+		someoneWins = true;
+	
+	// Andrew's cross
+	if ((a1.Text == a2.Text) && (b2.Text == c3.Text)){
+		someoneWins = true;
+		
+	}
+	if ((c1.Text == c2.Text) && (c2.Text == c3.Text))
+		someoneWins = true;
+	
+	// verticals
+	if ((a1.Text == b1.Text) && (b1.Text == c1.Text))
+		someoneWins = true;
+	if ((a2.Text == b2.Text) && (b2.Text == c2.Text))
+		someoneWins = true;
+	if ((a3.Text == b3.Text) && (b3.Text == c3.Text))
+		someoneWins = true;
+}
+
+public void printWinner(string winnerName){
+	
+}
+
+public void printWinner(bool turn){
+	disableOtherButtons();
+	char winner = 'X';
+	if (turn == true)
+		winner = 'O';
+	MessageBox.Show(winner + "wins!");
+}
+
+private void disableOtherButtons(){
+	try{
+		foreach (Control c in Controls){
+			Button b = (Button)c;
+			c.Enabled = false;
+		}// end foreach
+	}// end try 
+	catch (RuntimeException rEx){}
 }
